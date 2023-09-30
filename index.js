668ba67c5cd896a0262552cc669fa3270939ef8e
Allan Gazali <128587014+allangazali2804@users.noreply.github.com>
2023-09-30 20:50:48 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
-let backgroundMusic = new Audio("pokemonbgm.mp3");
-backgroundMusic.play();
+// Agar game memiliki musik di background, best practicenya adalah harus memiliki play and pause.
+var x = document.getElementById("myAudio");
 
+function playAudio() {
+  x.loop = true;
+  x.play();
+}
+
+function pauseAudio() {
+  x.pause();
+}
+
+window.addEventListener("load", function(){
+    x.play();
+    x.loop = true;
+})
+
+
+//Deklarasi variabel button
 var startButton = document.getElementById("button");
 var restartButton = document.getElementById("restart");
 
+// Event Listener untuk Start Button
 startButton.addEventListener("click", start)
 
 function start(){
@@ -12,19 +29,19 @@ function start(){
     document.querySelector(".player-1").innerHTML = player1;
     document.querySelector(".player-2").innerHTML = player2;
     var randomNumber1 = Math.floor(Math.random() * 10 + 1);
-    //var randomImage1 = "images/monster" + randomNumber1 + ".png";
+    var randomImage1 = "images/img-" + randomNumber1 + ".png";
     var randomNumber2 = Math.floor(Math.random() * 10 + 1);
-    //var randomImage2 = "images/monster" + randomNumber2 + ".png";
-    //document.querySelector(".img-1").setAttribute("src", randomImage1);
-    //document.querySelector(".img-2").setAttribute("src", randomImage2);
+    var randomImage2 = "images/img-" + randomNumber2 + ".png";
+    document.querySelector(".img-1").setAttribute("src", randomImage1);
+    document.querySelector(".img-2").setAttribute("src", randomImage2);
 
 let winner = "";
 if (randomNumber1 > randomNumber2){
     winner += player1 + " is the winner!";
-} else if(randomNumber1 = randomNumber2) {
-    winner += "Draw !";
-} else {
+} else if(randomNumber1 < randomNumber2) {
     winner += player2 + " is the winner!";
+} else {
+    winner += "Draw!";
 }
 document.getElementById("winner").innerHTML = winner;
 
@@ -32,9 +49,7 @@ document.getElementById("winner").innerHTML = winner;
     restartButton.innerHTML = "Click here to restart!"
 }
 
-
-
-
+// Event Listener untuk Restart Button
 restartButton.addEventListener("click", function(){
     restartButton.innerHTML = "";
     start();
